136bbf0065f179c5a55068ceeae9f30e3d028200
agdfydy <hakka.ydy.sub@gmail.com>
2022-07-02 07:26:08 +0900

    master

--- class/keyboard/init.lua
+++ class/keyboard/init.lua
@@ -10,20 +10,15 @@ function Keyboard:update( dt )
   -- -126 ~ -1: 離上
   -- 0: 押下のち離上
   -- 1 ~ 128: 押下
+
   if love.keyboard.isDown( self.key ) then
-    self.pressedframes = self.pressedframes <= 0 and 1 or
-                             math.min( self.pressedframes + 1,
-                                       KEY_PRESSED_MAX_FRAME )
+    self.pressedframes = self.pressedframes <= 0 and 1 or math.min( self.pressedframes + 1, KEY_PRESSED_MAX_FRAME )
   else
-    self.pressedframes = self.pressedframes > 0 and 0 or
-                             math.max( self.pressedframes - 1,
-                                       KEY_PRESSED_MIN_FRAME )
+    self.pressedframes = self.pressedframes > 0 and 0 or math.max( self.pressedframes - 1, KEY_PRESSED_MIN_FRAME )
   end
 
   -- 前提キーがあるならその押下判定を、ないなら無条件で true
-  local premisekeyCondition = self.premisekey and
-                                  love.keyboard.isDown( self.premisekey ) or
-                                  true
+  local premisekeyCondition = self.premisekey and love.keyboard.isDown( self.premisekey ) or true
 
   if premisekeyCondition then
     self.func( dt, self.pressedframes )
--- class/player.lua
+++ class/player.lua
@@ -1,6 +1,7 @@
 local Player = {}
 
 local PlainDebug = require( 'lib.debug' ):getInstance()
+local getTouches = love.touch.getTouches
 
 local Actor = require 'class.actor'
 local KeyManager = require 'class.keyboard.manager'
@@ -15,27 +16,52 @@ end
 local sprites = {}
 local image = love.graphics.newImage( data.player.sprite.image )
 for name, spritedata in pairs( data.player.sprite.frames ) do
-  sprites[name] = love.graphics.newQuad( spritedata.x, spritedata.y,
-                                         spritedata.w, spritedata.h,
-                                         image:getWidth(), image:getHeight() )
+  sprites[name] = love.graphics.newQuad( spritedata.x, spritedata.y, spritedata.w, spritedata.h, image:getWidth(), image:getHeight() )
 end
+
+local touchingFrames = { 0, 0, 0 }
+local touching = { false, false, false }
 
 function Player:draw()
   if self.direction > 0 then
     love.graphics.draw( image, sprites.idle, self.x, self.y )
   else
-    love.graphics.draw( image, sprites.idle, self.x + data.mapData[1].solidSize,
-                        self.y, 0, -1, 1 )
+    love.graphics.draw( image, sprites.idle, self.x + data.mapData[1].solidSize, self.y, 0, -1, 1 )
   end
 end
 
 function Player:update( dt )
   self._keyManager:update( dt )
+  local touches = getTouches()
+  for i, id in ipairs( touches ) do
+    local x, y = love.touch.getPosition( id )
+
+    touching[1] = i == 1 and x > love.graphics.getWidth() / 2
+    touching[2] = i == 1 and x < love.graphics.getWidth() / 2
+    touching[3] = i == 2
+  end
+
+  for i, t in ipairs( touching ) do
+    if t then
+      touchingFrames[i] = touchingFrames[i] > 60 and touchingFrames[i] or touchingFrames[i] + 1
+    else
+      touchingFrames[i] = touchingFrames[i] < -60 and touchingFrames[i] or touchingFrames[i] - 1
+    end
+  end
+
+  if #touches > 0 then
+    self._dash( dt, 1 )
+    self._left( dt, touchingFrames[2] )
+    self._right( dt, touchingFrames[1] )
+    self._jump( dt, touchingFrames[3] )
+  end
+
   if self.vx ~= 0 then
     self.actor:moveX( self.vx, function()
       self.vx = 0
     end )
   end
+
   if self.vx > 0.5 then
     self.direction = 1
   elseif self.vx < -0.5 then
@@ -151,8 +177,7 @@ function Player:new( x, y, w, h )
       if headbutting then
         obj.jumpframe = 0
       end
-    elseif ((f == 1 and not standing) or (f > 1 and standing)) and obj.jumpframe <=
-        0 then
+    elseif ((f == 1 and not standing) or (f > 1 and standing)) and obj.jumpframe <= 0 then
       obj.jumpframe = 0
     elseif (f <= 0 and standing) and obj.jumpframe <= 0 then
       obj.jumpframe = data.player.jumpframe
@@ -163,6 +188,74 @@ function Player:new( x, y, w, h )
 
   obj._keyManager = KeyManager:new()
   obj._keyManager:add( keyA, keyD, keyJ, keyK )
+
+  obj._left = function( dt, f )
+    local rate = dt * data.system.expectFPS
+    if f > 0 then
+      if obj.vx - obj.accel * rate < -obj.maxSpeed then
+        -- 加速しすぎる場合
+        obj.vx = -obj.maxSpeed
+      else
+        obj.vx = obj.vx - obj.accel * rate
+      end
+    else
+      if obj.vx >= 0 then
+        -- do nothing
+      elseif obj.vx + obj.accel * rate > 0 then
+        obj.vx = 0
+      else
+        obj.vx = obj.vx + obj.accel * rate
+      end
+    end
+  end
+  obj._right = function( dt, f )
+    local rate = dt * data.system.expectFPS
+    PlainDebug:setDebugInfo( tostring( obj.accel ) )
+    if f > 0 then
+      if obj.vx + obj.accel * rate > obj.maxSpeed then
+        -- 加速しすぎる場合
+        obj.vx = obj.maxSpeed
+      else
+        obj.vx = obj.vx + obj.accel * rate
+      end
+    else
+      -- 減速処理
+      if obj.vx <= 0 then
+        -- do nothing
+      elseif obj.vx - obj.accel * rate < 0 then
+        obj.vx = 0
+      else
+        obj.vx = obj.vx - obj.accel * rate
+      end
+    end
+  end
+  obj._dash = function( dt, f )
+    if f > 0 then
+      obj.maxSpeed = data.player.maxSpeedDash
+    else
+      obj.maxSpeed = data.player.maxSpeed
+    end
+  end
+  obj._jump = function( dt, f )
+    local standing = obj.actor:isStanding()
+    local headbutting = obj.actor:isHeadbutting()
+
+    if ((f == 1 and standing) or (f > 1 and not standing)) and obj.jumpframe > 0 then
+      obj.jumpframe = obj.jumpframe - 1
+      if obj.jumpframe > 0 then
+        obj.vy = -data.player.jumpforce
+      end
+      if headbutting then
+        obj.jumpframe = 0
+      end
+    elseif ((f == 1 and not standing) or (f > 1 and standing)) and obj.jumpframe <= 0 then
+      obj.jumpframe = 0
+    elseif (f <= 0 and standing) and obj.jumpframe <= 0 then
+      obj.jumpframe = data.player.jumpframe
+    elseif (f <= 0 and not standing) and obj.jumpframe > 0 then
+      obj.jumpframe = 0
+    end
+  end
 
   setmetatable( obj, { __index = Player } )
   return obj
